e8b81584c15c48e56d2232f2175c500ee8a5c7bf
Victoria Gramajo <marvi661@hotmail.com>
2024-09-06 21:15:54 -0300

    agregadoel formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import FormularioTarea from "./componentes/FormularioTarea"
+<componentes></componentes>;
 function App() {
-  return <>
-  </>;
+  return (
+    <>
+      <main className="my-5 container">
+        <h1 className="text-center">Bienvenidos</h1>
+        <h2>Ingresá tus tareas</h2>
+        <FormularioTarea></FormularioTarea>
+      </main>
+      {/*Agregar el maquetado del footer abajo*/}
+    </>
+  );
 }
 
 export default App;
