ee04e6cedd21179d2f21ad6b4fc1b12a20667b02
Victoria Gramajo <marvi661@hotmail.com>
2024-09-25 17:38:36 -0300

    agrego las etiqueta del index

--- src/componentes/ItemTarea.jsx
+++ src/componentes/ItemTarea.jsx
@@ -1,7 +1,7 @@
 import { ListGroup, Button } from "react-bootstrap";
 
-const ItemTarea = () => {
-  return <ListGroup.Item className="d-flex justify-content-between">Alguna tarea <Button variant="danger">Borrar</Button></ListGroup.Item>;
+const ItemTarea = ({tarea}) => {
+  return <ListGroup.Item className="d-flex justify-content-between">{tarea}<Button variant="danger">Borrar</Button></ListGroup.Item>;
 };
 
 export default ItemTarea;
